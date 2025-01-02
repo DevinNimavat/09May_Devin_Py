a01746cf2fe5d63a6d56474cfc6a98bbcc59ce02
DevinNimavat <devinnimavat888@gmail.com>
2025-01-02 10:13:54 +0530

    Python_Finel

--- Project/Hospital_Project/Hospital_Project/settings.py
+++ Project/Hospital_Project/Hospital_Project/settings.py
@@ -119,8 +119,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfile')
+
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+   os.path.join(BASE_DIR , "static")
 ]
 
 # Default primary key field type
